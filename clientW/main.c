5f2cd34564342353bd66182ccf68a2dedebab110
bbyg-0 <hizba.shafwatuddin.tif424@polban.ac.id>
2025-06-09 10:47:06 +0700

    client windows ama linux di 1 file

--- clientW/main.c
+++ clientW/main.c
@@ -1,18 +1,35 @@
 // File: client.c
 
+#ifdef _WIN32
 #include <winsock2.h>
-#include "../common/socket.h"
+#else
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+#endif
 
+#include "../common/socket.h"
+#define PORT 8080
+#define ADDRESS "127.0.0.1"
 int main() {
+	paramThread param;
+
+	inisialisasiParamThread(&param);
+	isiPort(&param, PORT);
+	isiAddress(&param, ADDRESS);
+	isiStatus(&param, 'C');
+
+
+#ifdef _WIN32
 	HANDLE cliSocket, sendMSG, getMSG;
 	DWORD cliSocketId, sendMSGId, getMSGId;
-	paramThread param;
 
 	WSADATA wsa;
 	WSAStartup(MAKEWORD(2,2), &wsa);
-
-
-	isiPort(&param, 8080);
 
 	cliSocket = CreateThread(NULL, 0, clientSocket, (LPVOID)&param, 0, &cliSocketId);
 	sendMSG = CreateThread(NULL, 0, sendMessage, (LPVOID)&param, 0, &sendMSGId);
@@ -27,6 +44,20 @@ int main() {
 
 	WaitForSingleObject(getMSG, INFINITE);
 	CloseHandle(getMSG);
+#else
+	pthread_t cliSocket, send, get;
+
+	pthread_create(&cliSocket, NULL, clientSocket, (void *)&param);
+	pthread_create(&send, NULL, sendMessage, (void *)&(param.clientSocket));
+	pthread_create(&get, NULL, getMessage, (void *)&param);
+
+	pthread_join(cliSocket, NULL);
+	pthread_join(send, NULL);
+	pthread_join(get, NULL);
+
+	// closing the connected socket
+	close((param).clientSocket);	
+#endif
 
 	return 0;
 }
